8221ac2b2a36b861b8b07ae58e6e6788fb5d4a82
Jeffrey Li <lijeffre@grinnell.edu>
2022-03-09 21:39:03 +0900

    Create hover DOM function

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const grid = document.querySelector(".grid");
 
 // create a function that adds the cell CSS class to the element and appends to the grid
-function addClass () {
+function addCell () {
     const cell = document.createElement("div");
     cell.classList.add("cell");
     grid.appendChild(cell);
@@ -11,8 +11,21 @@ function addClass () {
 // create a function that loops through 16x16 times calling addClass
 function draw() {
     for (let i = 0; i < 256; i++) {
-        addClass();
+        addCell();
     }
 }
 
 draw();
+
+// define a hover effect variable
+let hover = ".cell:hover{ background-color: blue }";
+
+// create a function that adds the :hover effect
+function addHover () {
+    const hoverCell = document.createElement("div");
+    hoverCell.classList.add("cell");
+    hoverCell.style.cssText = hover;
+    grid.appendChild(hoverCell);
+}
+
+// add event listener for when the mouse hovers over the div
