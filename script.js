b004ba39bb9b632a9f6d48abc14b7ddc2dbe11d2
Jeffrey Li <lijeffre@grinnell.edu>
2022-03-09 21:09:53 +0900

    Create addClass function

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
+// select the div with the class name grid
 const grid = document.querySelector(".grid");
 
+// create a function that adds the cell CSS class to the element and appends to the grid
+function addClass () {
+    const cell = document.createElement("div");
+    cell.classList.add("cell");
+    grid.appendChild(cell);
+}
