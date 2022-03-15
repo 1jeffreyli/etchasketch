aff83107d9fc21779060cf9d347eb5e0434d0956
Jeffrey Li <lijeffre@grinnell.edu>
2022-03-16 08:51:14 +0900

    Add basic grid functions

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 const container = document.getElementById("container");
+const rows = document.getElementsByClassName("containerRow");
 
 function drawRows(rowNum) {
     for (let i = 0; i < rowNum; i++) {
         let rowDiv = document.createElement("div");
-        rowDiv.className.add("cell");
-        container.appendChild(rowDiv);
+        container.appendChild(rowDiv).className = "containerRow";
     }
 }
+
+function drawColumns (colNum) {
+    for (let i = 0; i < rows.length; i++) {
+        for (let j = 0; j < colNum; j++) {
+            let colCell = document.createElement("div");
+            rows[j].appendChild(colCell).className = "square";
+        }
+    }
+}
+
+function drawGrid() {
+    drawRows(16);
+    drawColumns(16);
+}
+drawGrid();
