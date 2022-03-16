e76c375ee305641dadb344b5812cf74e88776d48
Jeffrey Li <lijeffre@grinnell.edu>
2022-03-16 12:51:12 +0900

    Refactor draw function to make it dynamic

--- script.js
+++ script.js
@@ -1,24 +1,14 @@
-const container = document.getElementById("container");
-const rows = document.getElementsByClassName("containerRow");
+const container = document.querySelector("#container");
 
-function drawRows(rowNum) {
-    for (let i = 0; i < rowNum; i++) {
-        let rowDiv = document.createElement("div");
-        container.appendChild(rowDiv).className = "containerRow";
-    }
-}
-
-function drawColumns (colNum) {
-    for (let i = 0; i < rows.length; i++) {
-        for (let j = 0; j < colNum; j++) {
-            let colCell = document.createElement("div");
-            rows[j].appendChild(colCell).className = "square";
+function draw(size) {
+    for (let i = 0; i < size; i++) {
+        const row = container.appendChild(document.createElement("div"));
+        for (let j = 0; j < size; j++) {
+            const cell = document.createElement("div");
+            cell.className = "square";
+            row.appendChild(cell);
         }
     }
 }
 
-function drawGrid() {
-    drawRows(16);
-    drawColumns(16);
-}
-drawGrid();
+draw(16);
