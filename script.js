78bdc091954f33d2f498a91bf414c97ff67257ac
Jeffrey Li <lijeffre@grinnell.edu>
2022-03-28 22:25:34 +0900

    Add basic grid styling and drawing functionality

---
+++ script.js
@@ -0,0 +1,13 @@
+const container = document.getElementById("container");
+
+function draw(rows, cols) {
+    container.style.setProperty("--grid-rows", rows);
+    container.style.setProperty("--grid-cols", cols);
+    for (let i = 0; i < (rows * cols); i++) {
+        let cell = document.createElement("div");
+        cell.innerText = (i + 1);
+        container.appendChild(cell).className = "cell";
+    }
+}
+
+draw(16, 16);
